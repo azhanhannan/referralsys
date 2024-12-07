903b744c8f3f6aa270220a8f647d2ebeedf4faa3
azhanhannan <azhanhannan@3fresources.com>
2024-12-08 02:04:53 +0800

    style: display red asterisk at required input label for clarity

--- resources/views/form/users.blade.php
+++ resources/views/form/users.blade.php
@@ -9,14 +9,14 @@
     <form action="{{ url('/users') }}" method="POST">
         @csrf
         <div class="mb-3">
-            <label for="name" class="form-label text-start d-block">Name</label>
+            <label for="name" class="form-label text-start d-block">Name <span style="color: red">*</span></label>
             <input type="text" name="name" id="name" class="form-control" placeholder="Enter user's name" required value="{{ old('name') }}">
             @error('name')
                 <div class="text-danger text-start d-block">{{ $message }}</div>
             @enderror
         </div>
         <div class="mb-3">
-            <label for="email" class="form-label text-start d-block">Email</label>
+            <label for="email" class="form-label text-start d-block">Email <span style="color: red">*</span></label>
             <input type="email" name="email" id="email" class="form-control" placeholder="Enter user's email" required autocomplete="new-password" value="{{ old('email') }}">
             @error('email')
                 <div class="text-danger text-start d-block">{{ $message }}</div>
@@ -27,7 +27,7 @@
             <input type="text" name="referral_code" id="referral_code" class="form-control" placeholder="Enter referral code (optional)">
         </div>
         <div class="mb-3">
-            <label for="password" class="form-label text-start d-block">Password</label>
+            <label for="password" class="form-label text-start d-block">Password <span style="color: red">*</span></label>
             <input type="password" name="password" id="password" class="form-control" placeholder="Enter password" required autocomplete="new-password" value="{{ old('password') }}">
             @error('password')
                 <div class="text-danger text-start d-block">{{ $message }}</div>
